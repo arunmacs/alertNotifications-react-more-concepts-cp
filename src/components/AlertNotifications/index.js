498af32af000a38db12df2e00629540e1f1c2ccd
arunmacs <aruncharymuniganti@gmail.com>
2021-08-16 14:05:31 +0000

    14/15 passed 1 fail

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,1 +1,39 @@
-// Write your code here
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning, MdInfo} from 'react-icons/md'
+
+import Notification from '../Notification'
+
+import './index.css'
+
+const AlertNotifications = () => (
+  <div className="container">
+    <h1>Alert Notifications</h1>
+    <Notification>
+      <AiFillCheckCircle className="success" />
+      <h1 className="success">Success</h1>
+      <p className="description">You can access all the files in the folder</p>
+    </Notification>
+    <Notification>
+      <RiErrorWarningFill className="error" />
+      <h1 className="error">Error</h1>
+      <p className="description">
+        Sorry, you are not authorized to have access to delete the file
+      </p>
+    </Notification>
+    <Notification>
+      <MdWarning className="warning" />
+      <h1 className="warning">Warning</h1>
+      <p className="description">
+        Viewers of this file can see comments and suggestions
+      </p>
+    </Notification>
+    <Notification>
+      <MdInfo className="info" />
+      <h1 className="info">Info</h1>
+      <p className="description">Anyone on the internet can view these files</p>
+    </Notification>
+  </div>
+)
+
+export default AlertNotifications
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,1 +1,24 @@
-// Write your code here
+import {GrFormClose} from 'react-icons/gr'
+
+import './index.css'
+
+const Notification = props => {
+  const {children} = props
+  console.log(children)
+
+  return (
+    <div className="notification-container">
+      <div className="icon-status-container">
+        <div>{children[0]}</div>
+        <p>{children[1]}</p>
+      </div>
+      <div className="description-icon-container">
+        <p>{children[2]}</p>
+        <div className="close-div">
+          <GrFormClose className="close" />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Notification
